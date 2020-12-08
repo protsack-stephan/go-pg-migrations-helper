9e0d733293a9048111eb6e8af3eea47e4223bdcd
Stephan Protsack <stephan@speedandfunction.com>
2020-12-08 12:50:01 +0200

    Moved from concatenation to fmt

--- column.go
+++ column.go
@@ -1,5 +1,7 @@
 package pgmigrations
 
+import "fmt"
+
 // Column struct to represent column in database
 type Column struct {
 	Table string
@@ -9,20 +11,20 @@ type Column struct {
 
 // Add creating new column inside the table
 func (column *Column) Add() string {
-	return "ALTER TABLE " + column.Table + " ADD COLUMN " + column.Define() + ";"
+	return fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s;", column.Table, column.Define())
 }
 
 // Drop deleting column from table
 func (column *Column) Drop() string {
-	return "ALTER TABLE " + column.Table + " DROP COLUMN " + column.Name + ";"
+	return fmt.Sprintf("ALTER TABLE %s DROP COLUMN %s;", column.Table, column.Name)
 }
 
 // Alter update column inside the table
 func (column *Column) Alter(sql string) string {
-	return "ALTER TABLE " + column.Table + " ALTER COLUMN " + column.Name + " " + sql + ";"
+	return fmt.Sprintf("ALTER TABLE %s ALTER COLUMN %s %s;", column.Table, column.Name, sql)
 }
 
 // Define describing column name and type
 func (column *Column) Define() string {
-	return column.Name + " " + column.Type
+	return fmt.Sprintf("%s %s", column.Name, column.Type)
 }
